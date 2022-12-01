b22c4165a243c7dd032ef1f1f276fc2a20b93606
J7K <do@not.use>
2022-12-01 18:44:44 +0100

    Day one - rough and ugly

---
+++ AoC2022_01/src/main.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::io::*;
+use std::fs::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!((args.len() > 1), "Missing input file argument");
+
+    let file = File::open(&args[1]).unwrap();
+    let lines = BufReader::new(file).lines();
+
+    // Compute calorie payload per elf
+    let mut accum: u32 = 0;
+    let mut elves_payload: Vec<u32> = Vec::new();
+    for line in lines  
+    {
+        let data: String = line.unwrap();
+        if data.is_empty()
+        {
+            elves_payload.push(accum);
+            accum = 0;
+        }
+        else
+        {
+            accum += data.parse::<u32>().unwrap();
+        }
+    }
+    // Account for end of file
+    elves_payload.push(accum);
+
+    // Find max carrying elf
+    let max_payload = elves_payload.iter().max().unwrap();
+
+    println!("Max carrying elf: {}", &max_payload);
+
+
+    assert!(elves_payload.len() >=3, "More than 3 elves are required as input" );
+    elves_payload.sort();
+    elves_payload.reverse();
+    let total_payload_top3 = elves_payload[0] + elves_payload[1] + elves_payload[2];
+
+    println!("Payload of top3 carrying elves: {}", total_payload_top3);
+
+}
+
