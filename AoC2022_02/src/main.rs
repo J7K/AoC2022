2308a6dd54fe746871a36e53aaec7f9b1ec33d39
J7K <do@not.use>
2022-12-02 09:55:04 +0100

    Day 2 - Still rough

---
+++ AoC2022_02/src/main.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+use std::env;
+use std::io::*;
+use std::fs::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!((args.len() > 1), "Missing input file argument");
+
+    let file = File::open(&args[1]).unwrap();
+    let lines: Vec<_> = BufReader::new(file)
+        .lines()
+        .filter_map(|x|x.ok())
+        .collect();
+
+    let score_silver: u32 = lines.iter()
+        .map(| line | round_score_silver(&line))
+        .sum();
+
+        let score_gold: u32 = lines.iter()
+        .map(| line | round_score_gold(&line))
+        .sum();
+
+    
+    println!("Silver Star - Final Score: {}", &score_silver);
+    println!("Gold Star - Final Score: {}", &score_gold);
+}
+
+fn round_score_silver(round: &String) -> u32
+{
+    let round_outcome: HashMap<_,u32> =
+    [ 
+        ("A X", 4), ("A Y", 8), ("A Z", 3),
+        ("B X", 1), ("B Y", 5), ("B Z", 9),
+        ("C X", 7), ("C Y", 2), ("C Z", 6)
+    ].into_iter().collect();
+    
+    return *round_outcome.get(round.as_str()).unwrap();
+}
+
+fn round_score_gold(round: &String) -> u32
+{
+    let round_outcome: HashMap<_,u32> =
+    [ 
+        ("A X", 3), ("A Y", 4), ("A Z", 8),
+        ("B X", 1), ("B Y", 5), ("B Z", 9),
+        ("C X", 2), ("C Y", 6), ("C Z", 7)
+    ].into_iter().collect();
+    
+    return *round_outcome.get(round.as_str()).unwrap();
+}
